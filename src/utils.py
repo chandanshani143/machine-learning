1d4d8e03872b4e04c8f0167fad9061302277385f
Chandan Kumar Shani <chandankumarshani98@gmail.com>
2025-11-01 10:00:00 +0100

    Hyperparameter training done

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import numpy as np
 import dill
 # import pickle
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -24,14 +25,21 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
     
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para = param[list(models.keys())[i]]       #ex:- param["Random Forest"] -> para = {'n_estimators': [8,16,32,64,128,256]}
 
-            # Train the model
+            gs = GridSearchCV(model, para, cv=3)    #it will test every combination of parameters with 3 cross validation
+            gs.fit(X_train, y_train)                #Trains the model with every parameter combination and finds the best one
+
+            #setting the best params found to our model
+            model.set_params(**gs.best_params_)
+
+            # Train final model with best parameters
             model.fit(X_train, y_train)
 
             y_train_pred = model.predict(X_train)
